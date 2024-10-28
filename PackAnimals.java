5897ba1c0839a57c3e475bfa417b0389b5f9ccb9
Maxim <Crawn@crawn.ru>
2024-10-28 11:42:12 +0300

    Создал файл и класс Pets

---
+++ PackAnimals.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public abstract class PackAnimals extends Animal {
+    public PackAnimals(String name, LocalDate birthDate) {
+        super(name, birthDate);
+    }
+}
