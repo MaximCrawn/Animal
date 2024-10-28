b03b0e4f64ea984a2609375ee5505393df440fe6
Maxim <Crawn@crawn.ru>
2024-10-28 11:34:52 +0300

    Создал класс Animal c методами

--- Animal.java
+++ Animal.java
@@ -1,3 +1,35 @@
-public class Animal {
-    
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Animal {
+    private String name;
+    private LocalDate birthDate;
+    protected List<String> commands;
+
+    public Animal(String name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.commands = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public List<String> getCommands() {
+        return new ArrayList<>(commands);
+    }
+
+    public void addCommand(String command) {
+        this.commands.add(command);
+    }
+
+    public void addCommands(List<String> newCommands) {
+        this.commands.addAll(newCommands);
+    }
 }
